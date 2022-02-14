ba7d69ec7629a32eb36c8ca838793167cf8bda77
Nati <natalia@savvyy.ai>
2022-02-13 19:41:38 -0500

    initial: initial commit

---
+++ apps/web/src/app/app.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'lazy-dashboard-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent {
+  title = 'web';
+}
