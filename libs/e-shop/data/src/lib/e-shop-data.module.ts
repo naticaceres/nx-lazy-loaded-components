3dc201ac4bef99dbfce786de9521cf76a2353747
Nati <natalia@savvyy.ai>
2022-06-13 11:30:02 -0400

    chore: install in memory api

--- libs/e-shop/data/src/lib/e-shop-data.module.ts
+++ libs/e-shop/data/src/lib/e-shop-data.module.ts
@@ -4,9 +4,14 @@ import { StoreModule } from '@ngrx/store';
 import { EffectsModule } from '@ngrx/effects';
 import * as fromEShopData from './+state/data.reducer';
 import { EShopDataEffects } from './+state/data.effects';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   imports: [
+    HttpClientModule,
+
+    // environment.production ?
+    //   HttpClientInMemoryWebApiModule.forRoot(InMemoryDb, { delay: 100 }) : []
     CommonModule,
     StoreModule.forFeature(
       fromEShopData.E_SHOP_DATA_FEATURE_KEY,
